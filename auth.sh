765f63c0b3f370216b1e36dc6cd3fbe34d2d0b6e
MoekrieJawa <sategagak123@gmail.com>
2025-02-08 18:24:14 +0700

    Update Add auth.sh

---
+++ auth.sh
@@ -0,0 +1,4 @@
+export TWILIO_ACCOUNT_SID="Your Twilio Account SID"
+export TWILIO_AUTH_TOKEN="Your Twilio Account Token"
+export TWILIO_NUMBER="Your Twilio Buyed Number"
+export TARGET_NUMBER="Target Number" #only for verified number
