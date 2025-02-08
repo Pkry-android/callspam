65982919b77bf6e0f00f4ebc6d70aca0b4787590
MoekrieJawa <sategagak123@gmail.com>
2025-02-08 18:23:35 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
-sudo apt -y update
-sudo apt -y upgrade
 python -m venv call
 source call/bin/activate
 pip3 install -r requirements.txt
-export TWILIO_ACCOUNT_SID="Your Twilio Account SID"
-export TWILIO_AUTH_TOKEN="Your Twilio Account Token"
-export TWILIO_NUMBER="Your Twilio Buyed Number"
-export TARGET_NUMBER="Target Number" #only for verified number
+bash auth.sh
 python call.py
